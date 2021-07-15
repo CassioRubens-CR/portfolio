43edd79536ed9daebcbd753e553a146b438fb933
Cássio Rubens <cassiorubensadv@gmail.com>
2021-07-15 18:16:48 -0300

    update text about

--- src/pages/About.js
+++ src/pages/About.js
@@ -14,19 +14,21 @@ export function About() {
           alt='cassio'
         />
         <h1>Cássio Rubens</h1>
-        <p>
-          Cada passo na área de tecnologia é uma conquista, trabalhar em equipe, compartilhar conhecimento, soft skills, resolver problemas, certamente são grandes pilares que fazem parte da vida de um desenvolvedor em busca de evoluir cada dia, o que me permite resumir os meus objetivos.   
-        </p>
-        <p>
-          Sou formado em Direito onde exerci por nove anos a profissão de Advogado e por sentir insatisfeito profissionalmente e possuir uma certa admiração pela área de tecnologia resolvi fazer transição de carreira.     
-        </p>
-        <p>
-          Conclui o curso desenvolvimento software Full Stack na Trybe ministrado com mais de 1000 horas, abordando Front-end, Back-end, engenharia de software e metodologias agile. No decorrer do curso foram abordada as seguintes tecnologias:
-          Git - Github - Javascript - Python - FrontEnd (HTML, CSS, ReactJS) - BackEnd (NodeJS, MongoDB, SQL, MySQL, ExpressJS) - Unit-tests, Principios SOLID - DDD - TDD.
-        </p>
-        <p>
-          Desde então, busco a cada dia minha melhor versão.
-        </p>
+          <div className="text-about">
+            <p>
+              Cada passo na área de tecnologia é uma conquista, trabalhar em equipe, compartilhar conhecimento, soft skills, resolver problemas, certamente são grandes pilares que fazem parte da vida de um desenvolvedor em busca de evoluir cada dia, o que me permite resumir os meus objetivos.   
+            </p>
+            <p>
+              Sou formado em Direito onde exerci por nove anos a profissão de Advogado e por sentir insatisfeito profissionalmente e possuir uma certa admiração pela área de tecnologia resolvi fazer transição de carreira.     
+            </p>
+            <p>
+              Conclui o curso desenvolvimento software Full Stack na Trybe ministrado com mais de 1000 horas, abordando Front-end, Back-end, engenharia de software e metodologias agile. No decorrer do curso foram abordada as seguintes tecnologias:
+              Git - Github - Javascript - Python - FrontEnd (HTML, CSS, ReactJS) - BackEnd (NodeJS, MongoDB, SQL, MySQL, ExpressJS) - Unit-tests, Principios SOLID - DDD - TDD.
+            </p>
+            <p>
+              Desde então, busco a cada dia minha melhor versão.
+            </p>
+          </div>
       </div>
     </div>
   )
